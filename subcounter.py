1901103adc40200a0eaed8b61bd0a4ded1563d9f
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-19 20:10:09 +0200

    Update subcounter.py

--- subcounter.py
+++ subcounter.py
@@ -1,5 +1,22 @@
 from bs4 import BeautifulSoup
+import os
+import dotenv as dv
+import requests
+
+dv.load_dotenv()
+USER = os.getenv("MYUSER")
+print(USER)
 
 
-def subcounter():
-    pass
+def subcounter(user, platform):
+    if platform == "youtube":
+        url = f"https://youtube.com/@{user}"
+        r = requests.get(url=url)
+        soup = BeautifulSoup(r.content, features="html.parser")
+        x = soup.find_all(name="div")
+        for i in x:
+            print(i)
+
+if __name__ == "__main__":
+    platform = "youtube"
+    subcounter(user=USER, platform=platform)
