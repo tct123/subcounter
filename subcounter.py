d64daa7581168ccae7af47314e67d32c580118c4
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-19 23:57:10 +0200

    Update subcounter.py

--- subcounter.py
+++ subcounter.py
@@ -12,11 +12,11 @@ def subcounter(user, platform):
     if platform == "instagram":
         L = Instaloader()
         profile = Profile.from_username(L.context, user)
-        print(profile.followers)
+        return profile.followers
 
 
 if __name__ == "__main__":
     dv.load_dotenv()
     USER = os.getenv("MYUSER")
     platform = "instagram"
-    subcounter(user=USER, platform=platform)
+    print(subcounter(user=USER, platform=platform))
