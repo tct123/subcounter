6698bd553efb801f6ad876c4b27ca0128546ab00
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-20 00:41:27 +0200

    .

--- subcounter.py
+++ subcounter.py
@@ -2,13 +2,19 @@ import os
 import dotenv as dv
 from pytube import Channel
 from instaloader import Instaloader, Profile
+from bs4 import BeautifulSoup
 
 
 def subcounter(user, platform):
     if platform == "youtube":
-        url = f"https://youtube.com/c/{user}"
+        url = f"https://youtube.com/channel/{user}"
         channel = Channel(url)
-        channel.channel_name
+        html = channel.html
+        bs = BeautifulSoup(html, "html.parser")
+        counter = bs.find_all()
+        for x in range(0, len(counter)):
+            print(counter[x])
+        return counter
     if platform == "instagram":
         L = Instaloader()
         profile = Profile.from_username(L.context, user)
@@ -18,5 +24,5 @@ def subcounter(user, platform):
 if __name__ == "__main__":
     dv.load_dotenv()
     USER = os.getenv("MYUSER")
-    platform = "instagram"
+    platform = "youtube"
     print(subcounter(user=USER, platform=platform))
