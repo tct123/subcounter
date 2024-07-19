9c551fed1b6c814a085feb1d5bf7d2d4463264d6
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-19 21:06:29 +0200

    .

--- subcounter.py
+++ subcounter.py
@@ -11,11 +11,15 @@ print(USER)
 def subcounter(user, platform):
     if platform == "youtube":
         url = f"https://youtube.com/@{user}"
-        r = requests.get(url=url)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        }
+        r = requests.get(url=url, headers=headers)
         soup = BeautifulSoup(r.content, features="html.parser")
         x = soup.find_all(name="div")
         for i in x:
             print(i)
+
 
 if __name__ == "__main__":
     platform = "youtube"
